1147d426d3eacb668126f18f1e52357b746da81b
TChidiDev <124631882+TChidiDev@users.noreply.github.com>
2025-11-08 10:11:50 +0100

    Reformat bot.py with consistent indentation

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os
+# import os
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -32,6 +32,7 @@ async def hello(interaction: discord.Interaction):
 # Run your bot
 bot.run("RlBqCxyhM1kGCxQU0ivp31F-qCipNnYi")
 # bot.run(os.getenv("TOKEN"))
+
 
 
 
